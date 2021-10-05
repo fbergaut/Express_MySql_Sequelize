a4efaafbedd98e4527dc70aee13f2e47623c790d
fbergaut <f.bergaut@gmail.com>
2021-10-05 12:50:40 +0200

    INstall Sequelize in server.js

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const routes = require("./routes");
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize('database', 'root', 'developper', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+(async() => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})()
+
 
 routes(app);
 
