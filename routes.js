2d40196d9a9c749c6931f51aeded69efe4d54806
fbergaut <f.bergaut@gmail.com>
2021-10-15 09:56:02 +0200

    Correctif

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("streamers", "root", "Nopainnogain21*", {
+const sequelize = new Sequelize("streamers", "root", "*********", {
     host: "localhost",
     dialect: "mysql",
 });
